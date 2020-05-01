7b38d23468d48e3a526ec4b7b9e86e518823949a
Samarna <63645732+Samarna@users.noreply.github.com>
2020-05-01 08:12:42 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,26 +1,28 @@
 var wall;
 var bullet;
+
 var width;
 var speed;
+
 var weight;
 var damage;
 
 function setup() {
   createCanvas(1600,400);
-   bullet = createSprite(200, 200, 20, 20);
-   bullet.shapeColor = "white";
-  
    wall = createSprite(1500, 200, width, 400);
    wall.shapeColor = (80,80,80);
+  
+  bullet = new bullet class(weight,speed);
+  
+  width = Math.round(randomNumber(22,83));
+  weight = Math.round(randomNumber(30,52));
+  speed = Math.round(randomNumber(223,321));
 }
 
 function draw() {
 background("grey");
-  width = Math.round(randomNumber(22,83));
-  weight = Math.round(randomNumber(30,52));
-  speed = Math.round(randomNumber(223,321));
-  
   damage = 0.5*weight*speed*speed/width*width*width;
+  
   if(damage<10){
     wall.shapeColor = "green";
   }
