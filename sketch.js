36d1e599cd586d4f8326bc4d2f00a87f2c311ad7
Samarna <63645732+Samarna@users.noreply.github.com>
2020-05-13 10:03:59 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -37,7 +37,7 @@ background("grey");
 function hasCollided(lbullet,lwall){
   bulletRightEdge = lbullet.x + lbullet.width;
   wallLeftEdge = lwall.x;
-  if(lbulletRightEdge >= wallLeftEdge){
+  if(bulletRightEdge >= wallLeftEdge){
     return true
   }
   return false;
